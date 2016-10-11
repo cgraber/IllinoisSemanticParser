f3df737c6d44eb7296aeb6572fc146d51b7ae16a
Colin Graber <cgraber2@illinois.edu>
2016-10-11 13:01:33 -0500

    Cleaining up blocksworld data

--- data/BlocksWorld/convertBlocks.py
+++ data/BlocksWorld/convertBlocks.py
@@ -2,18 +2,22 @@ import sys
 from nltk import word_tokenize
 
 if len(sys.argv) != 3:
-    print "Usage: python convertGeo.py <input file> <output file>"
+    print "Usage: python convertBlocks.py <input file> <output file>"
     sys.exit(1)
 
 with open(sys.argv[1], "r") as fin:
     result = []
+    current = []
+    result.append(current)
     sent = None
     formula = None
     config = None
     tempConfig = []
     for line in fin:
         if not line.strip():
-            continue
+            current = []
+            result.append(current)
+            config = None
         elif line.strip() == "===":
             config = tempConfig
             tempConfig = []
@@ -25,16 +29,16 @@ with open(sys.argv[1], "r") as fin:
             formula.append("</s>")
         else:
             sent = word_tokenize(line.strip())
-            sent = [x.lower() for x in sent]
             sent.insert(0, "<s>")
             sent[-1] = "</s>" # Note that we replace the end-of-sentence full stop with this
-            result.append((config, formula, sent))
+            current.append((config, formula, sent))
             sent = None
             formula = None
-            config = None
 
 with open(sys.argv[2], "w") as fout:
-    for config, formula, sent in result:
-        fout.write("%s===%s\n"%(" ".join(sent), " ".join(formula)))
+    for current in result:
+        for config, formula, sent in current:
+            fout.write("%s===%s\n"%(" ".join(sent), " ".join(formula)))
+        fout.write("\n")
         
 
